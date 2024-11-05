a882ddeadc8ab3b6a039d13fb86c558ee06f0ac3
Nick_2002b <nicolae.balaban@studio.unibo.it>
2024-11-05 19:48:04 +0100

    Implement all methods on StrickBankAccount.java

--- 01-interfaces/src/main/java/it/unibo/encapsulation/interfaces/StrictBankAccount.java
+++ 01-interfaces/src/main/java/it/unibo/encapsulation/interfaces/StrictBankAccount.java
@@ -1,5 +1,62 @@
 package it.unibo.encapsulation.interfaces;
 
-public class StrictBankAccount {
+public class StrictBankAccount implements BankAccount {
+
+    private double balance;
+    private int transactions;
+    private static double ATM_TRANSACTION_FEE = 1;
+    private int id;
+    private static double FIXED_FEE = 5;
+    private static double TRANSACTION_FEE = 0.1;
+
+    StrictBankAccount(final int id, final double amount){
+        this.id = id;
+        this.balance = amount;
+        this.transactions = 0;
+    }
+
+    public void withdraw(int id, double amount) {
+        if(id == this.id && amount <= this.balance) {
+            this.balance -= amount;
+            transactions++;
+        }
+    }
+
+    public void deposit(int id, double amount) {
+        if(id == this.id) {
+            this.balance += amount;
+            transactions++;
+        }
+    }
+
+    public void depositFromATM(int id, double amount) {
+        if(id == this.id) {
+            this.balance += (amount - ATM_TRANSACTION_FEE);
+            transactions++;
+        }
+    }
+
+    public void withdrawFromATM(int id, double amount) {
+        if(id == this.id && amount <= this.balance) {
+            this.balance -= (amount - ATM_TRANSACTION_FEE);
+            transactions++;
+        }
+    }
+
+    public void chargeManagementFees(int id) {
+        double totalFee = FIXED_FEE + (TRANSACTION_FEE * this.transactions);
+        if(this.id == id && this.balance >= totalFee){
+            this.balance -= totalFee;
+            this.transactions = 0;
+        }
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public int getTransactionsCount() {
+        return this.transactions;
+    }
     
 }
