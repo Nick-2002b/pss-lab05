5a0d7db6701a99f9f60079b852c113fb0df3fad2
Nick_2002b <nicolae.balaban@studio.unibo.it>
2024-11-05 21:00:02 +0100

    Implement toString() in Professor.java

--- 02-composition/src/main/java/it/unibo/composition/Professor.java
+++ 02-composition/src/main/java/it/unibo/composition/Professor.java
@@ -1,23 +1,29 @@
 package it.unibo.composition;
 
+import java.util.Arrays;
+
 public class Professor implements User{
+    private static final String DOT = ".";
     private final int id;
     private String name;
     private String surname;
     private String password;
     private String[] courses;
     
-    Professor(int id, String name, String surname, String password){
+    Professor(int id, String name, String surname, String password, String[] cours){
         this.id = id;
         this.name = name;
         this.surname = surname;
         this.password = password;
+        this.courses = cours;
     }
 
-
+    public void replaceCourse(String cours, int index){
+        
+    }
     
     public String getUsername() {
-        return this.name;
+        return this.name + Professor.DOT + this.surname;
     }
 
     
@@ -25,9 +31,18 @@ public class Professor implements User{
         return this.password;
     }
 
+    public String toString(){
+        return "Professor [" 
+        + "name= " + this.name 
+        + ", surname= " + this.surname 
+        + ", id=" + this.id
+        + ", subjects=" + Arrays.toString(this.courses)
+        + "]";
+    }
+
     
     public String getDescription() {
-        return null;
+        return this.toString();
     }
     
 }
